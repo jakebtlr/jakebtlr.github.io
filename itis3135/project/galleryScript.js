fd7660d7fa5a91fedaaedc687bb17327fb3e5458
jakebtlr <jbutle83@charlotte.edu>
2024-12-04 19:06:05 -0500

    ACUTUAL final

--- itis3135/project/galleryScript.js
+++ itis3135/project/galleryScript.js
@@ -1,9 +1,16 @@
-const images = ["gallery-image-1.jpg", "gallery-image-2.jpg", "gallery-image-3.jpg", "gallery-image-4.jpg", "gallery-image-5.jpg", "gallery-image-6.jpg", 
-"gallery-image-7.jpg", "gallery-image-8.jpg", "gallery-image-9.jpg", "gallery-image-10.jpg", "gallery-image-11.jpg", "gallery-image-12.jpg", 
-"gallery-image-13.jpg", "gallery-image-14.jpg", "gallery-image-15.jpg", "gallery-image-16.jpg", "gallery-image-17.jpg", "gallery-image-18.jpg"]; 
-let currentIndex = 0;
+const images = [
+    "gallery-image-1.jpg", "gallery-image-2.jpg", "gallery-image-3.jpg", 
+    "gallery-image-4.jpg", "gallery-image-5.jpg", "gallery-image-6.jpg", 
+    "gallery-image-7.jpg", "gallery-image-8.jpg", "gallery-image-9.jpg", 
+    "gallery-image-10.jpg", "gallery-image-11.jpg", "gallery-image-12.jpg", 
+    "gallery-image-13.jpg", "gallery-image-14.jpg", "gallery-image-15.jpg", 
+    "gallery-image-16.jpg", "gallery-image-17.jpg", "gallery-image-18.jpg"
+]; 
 
+let currentIndex = 0;
 let modalOpen = false;
+
+// Function declarations
 
 function showImage(index) {
     currentIndex = index;
@@ -25,13 +32,13 @@ function closeImage() {
 }
 
 function nextImage(event) {
-    event.stopPropagation();
+    if (event) event.stopPropagation();
     currentIndex = (currentIndex + 1) % images.length;
     document.getElementById('fullImage').src = images[currentIndex];
 }
 
 function prevImage(event) {
-    event.stopPropagation();
+    if (event) event.stopPropagation();
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     document.getElementById('fullImage').src = images[currentIndex];
 }
